3aad61c205dbbedcc7d8f05b64f6a2dea57f0ea8
Marco Rossini <marco.rossini.1994@gmail.com>
2021-03-30 19:13:20 +0200

    Improved search effectiveness

--- movies_es/app.py
+++ movies_es/app.py
@@ -15,94 +15,61 @@ def search_request():
     search_term = request.form["input"]
     if request.form["search"] == "imdb-search":
         search_mode = "imdb-boost"
-        res = es.search(
-            index="movies",
-            size=20,
-            body={
-                "query": {
-                    "function_score": {
-                        "query": {
-                            "multi_match": {
-                                "query": search_term,
-                                "fields": [
-                                    "plot"
-                                ]
-                            }
-                        },
-                        "script_score": {
-                            "script": {
-                                "source": "doc.containsKey('imdb.rating') && doc['imdb.rating'].value > 0.0 ? doc['imdb.rating'].value : 1"
-                            }
-                        }
-                    }
-                },
-                "highlight": {
-                    "fields": {
-                        "plot": {}
-                    },
-                    "pre_tags": ["<b>"],
-                    "post_tags": ["</b>"],
-                    "fragment_size": 5000
-                }
-            }
-        )
-    elif request.form["search"] == "tomatoes-search":
-        search_mode = "tomatoes-boost"
-        res = es.search(
-            index="movies",
-            size=20,
-            body={
-                "query": {
-                    "function_score": {
-                        "query": {
-                            "multi_match": {
-                                "query": search_term,
-                                "fields": [
-                                    "plot"
-                                ]
-                            }
-                        },
-                        "script_score": {
-                            "script": {
-                                "source": "doc.containsKey('tomatoes.viewer.rating') && doc['tomatoes.viewer.rating'].value > 0.0 ? doc['tomatoes.viewer.rating'].value : 1"
-                            }
-                        }
-                    }
-                },
-                "highlight": {
-                    "fields": {
-                        "plot": {}
-                    },
-                    "pre_tags": ["<b>"],
-                    "post_tags": ["</b>"],
-                    "fragment_size": 5000
-                }
-            }
-        )
-    else:
-        search_mode = "no-boost"
-        res = es.search(
-            index="movies",
-            size=20,
-            body={
+        query = {
+            "function_score": {
                 "query": {
                     "multi_match": {
                         "query": search_term,
-                        "fields": [
-                            "plot",
-                        ]
+                        "fields": ["title", "fullplot"]
                     }
                 },
-                "highlight": {
-                    "fields": {
-                        "plot": {}
-                    },
-                    "pre_tags": ["<b>"],
-                    "post_tags": ["</b>"],
-                    "fragment_size": 5000
+                "script_score": {
+                    "script": {
+                        "source": "doc.containsKey('imdb.rating') && doc['imdb.rating'].value > 0.0 ? doc['imdb.rating'].value : 1"
+                    }
                 }
             }
-        )
+        }
+    elif request.form["search"] == "tomatoes-search":
+        search_mode = "tomatoes-boost"
+        query = {
+            "function_score": {
+                "query": {
+                    "multi_match": {
+                        "query": search_term,
+                        "fields": ["title", "fullplot"]
+                    }
+                },
+                "script_score": {
+                    "script": {
+                        "source": "doc.containsKey('tomatoes.viewer.rating') && doc['tomatoes.viewer.rating'].value > 0.0 ? doc['tomatoes.viewer.rating'].value : 1"
+                    }
+                }
+            }
+        }
+    else:
+        search_mode = "no-boost"
+        query = {
+            "multi_match": {
+                "query": search_term,
+                "fields": ["title", "fullplot"]
+            }
+        }
+
+    highlight = {
+        "fields": {
+            "fullplot": {}
+        },
+        "pre_tags": ["<b>"],
+        "post_tags": ["</b>"],
+        "fragment_size": 5000
+    }
+
+    res = es.search(
+        index="movies",
+        size=20,
+        body={"query": query, "highlight": highlight},
+    )
     return render_template("results.html", res=res, search_mode=search_mode)
 
 
